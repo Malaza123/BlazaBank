f80b843daa8e9302861291104fd216ab99fe8629
Malaza123 <bongani@icurity.co.za>
2023-06-02 21:27:24 +0200

    iupdate

---
+++ src/main/java/com/blazabank/helpers/Token.java
@@ -0,0 +1,11 @@
+package com.blazabank.helpers;
+
+import java.util.UUID;
+
+public class Token {
+
+    public static String generateToken(){
+        String token = UUID.randomUUID().toString();
+        return token;
+    }
+}
